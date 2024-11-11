711ce2e1c67464043e9d9f5b33d9569890412fda
Daniel Ganchev <dani.ganchev1989@gmail.com>
2024-11-11 13:24:27 +0200

    setup project

---
+++ src/main/groovy/net/dodo/jwtimplementation/JwtImplementationApplication.groovy
@@ -0,0 +1,13 @@
+package net.dodo.jwtimplementation
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JwtImplementationApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JwtImplementationApplication, args)
+    }
+
+}
